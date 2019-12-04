7cfdb47d5caa8ca3667962f3987d12e9419fe345
sausenj <sausen.jessa@gmail.com>
2019-12-04 15:31:50 +0100

    Add files via upload
    line following works for basic cases but not for intersections, dead ends or if it gets to the white parts with no line

--- 5SENSORLINEFOLLOW.ino
+++ 5SENSORLINEFOLLOW.ino
@@ -1,20 +1,13 @@
 #include<Servo.h>
 Servo leftServo;          // Define left servo
 Servo servoRight;         // Define right servo
-Servo gripper;
-Servo arm_gripper;
 
 #include <QTRSensors.h>         //Pololu QTR Sensor Library
 
-
 /*------ Arduino Line Follower Code----- */
 /*-------definning Inputs------*/
-#define LS       // left sensor
-#define MS 9
-#define RS 8      // right sensor
-
-#define trigpin 12
-#define echo 13
+#define trigpin 7
+#define echo 6
 
 /*-------definning Outputs------*/
 #define LM 3       // left motor
@@ -28,18 +21,24 @@ Servo arm_gripper;
 #define NUM_SAMPLES_PER_SENSOR  4  // average 4 analog samples per sensor reading
 #define EMITTER_PIN             QTR_NO_EMITTER_PIN  // emitter is controlled by digital pin 2
 
-// sensors 0 through 5 are connected to analog inputs 0 through 5, respectively
+// sensors 0 through 5 are connected to analog inputs 0 through 4, respectively
 QTRSensorsAnalog qtra((unsigned char[]) {
   0, 1, 2, 3, 4
 }, NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
 unsigned int sensorValues[NUM_SENSORS];
 
+// Keeping track of distance and turning decisions
+int distance;
+#define ARRAY_SIZE 5;
+int decisions[] = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+int decisionsCount;
+int nextMove;
+long duration;
+
 void setup()
 {
   leftServo.attach(LM);  // Set left servo to digital pin 4
   servoRight.attach(RM);  // Set right servo to digital pin 3
-  gripper.attach(9);  // attaches the servo on digital pin 9 
-  arm_gripper.attach(10);  // attaches the servo on digital pin 10
 
   Serial.begin(9600);    //Open serial port and set this baudrate
 
@@ -48,6 +47,8 @@ void setup()
 
   leftServo.write(90);
   servoRight.write(90);
+
+  delay(5000);
 
   for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
   {
@@ -74,250 +75,90 @@ void setup()
   delay(1000);
 }
 
-int distance;
-int cylinder = 0;
 void loop()
 {
+  //use sensors to follow the line and calculate position
+  //from right to left the position varies from 0 - 4000 with the middle at 2000
+  qtra.read(sensorValues);             //Read Sensors
   unsigned int position = qtra.readLine(sensorValues);
 
-  if (position > 1500 && position < 2500) // position is around 1000
-  {
-    forward();
-    Serial.println ("Forward: ");
-    Serial.println(position);
-  }
-  else if (sensorValues[0] < 500 && sensorValues[2] < 500 && sensorValues[1] < 500 && sensorValues[3] < 500 && sensorValues[4] < 500) // stop if all are on white
-  {
-    //stopRobot ();
-    Serial.print("All white");
-    //ultrasonic sensor
-    if (distance_front > dist_limit && distance_right > dist_limit && distance_left > dist_limit){
-      foward();
-    } else if(distance_front < dist_limit && distance_right < dist_limit && distance_left < dist_limit){
-      reverse();
-    } else if(distance_front < dist_limit && distance_right < dist_limit && distance_left > dist_limit){
-      turnLeft();
-      delay(1000);//MODIFY
-      foward();
-    } else if(distance_front < dist_limit && distance_right > dist_limit && distance_left < dist_limit){
-      turnRight();
-      delay(1000);//MODIFY
-      foward();
-    }
-  }
-  else if (sensorValues[0] > 500 && sensorValues[2] > 500 && sensorValues[1] > 500 && sensorValues[3] > 500 && sensorValues[4] > 500) // stop if all are on black
-  {
-//    stopRobot ();
-//    Serial.print("All black stop");
-      forward();
-      delay(50);
-      if (sensorValues[0] < 500 && sensorValues[2] < 500 && sensorValues[1] < 500 && sensorValues[3] < 500 && sensorValues[4] < 500) // stop. T junction
-      {
-        Serial.print("All white");
-        reverse();
-        delay(50);
-        path();
-      }
-      else if (position > 1500 && position < 2500) // Cross junction
-      {
-        path();
-      }
-  }
-  else if (position > 3500) // the line is on the left
-  {
-    forward();
-    delay(50);
-    path();
-    //turnRight();
-    delay(150);
-    Serial.println ("Left turn: " );
-    Serial.println (position);
-  }
-  else if (position < 2000)  // the line is on the right
-  {
-    forward();
-    delay(50);
-    path();
-    //turnLeft();
-    delay(150);
-    Serial.println ("Right turn: ");
-    Serial.println(position);
-  }
-
-  //if proximity sensor find a cylinder
-  //stopRobot();
-  //grip();
-  //cylinder = cylinder + 1;
-  //if(cylinder == 3){exit_path();}
-}
-
-// Motion routines for forward, reverse, turns, and stop
-void forward() {
-  leftServo.write(180);
-  servoRight.write(0);
-}
-
-void reverse() {
-  leftServo.write(0);
-  servoRight.write(180);
-}
-
-void turnRight() {
-  leftServo.write(180);
-  servoRight.write(90);
-}
-void turnLeft() {
-  leftServo.write(90);
-  servoRight.write(0);
-}
-
-void stopRobot() {
-  leftServo.write(90);
-  servoRight.write(90);
-}
-
-void findDistance()
-{
-  int duration;
-  digitalWrite(trigpin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigpin, LOW);
-
-  duration = pulseIn(echo, HIGH);
-  distance = (1 / 29.1) * (duration / 2);
-  delay(60);
-}
-
-void grip(){
-  int pos_grip=0;
-  int pos_arm=0;
-  for (pos_arm = 95; pos_arm >= 0; pos_arm -= 1) { // arm goes from 95 degrees to 0 degrees
-    // in steps of 1 degree
-    gripper.write(pos_arm);              // tell servo to go to position in variable 'pos_grip'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos_grip = 0; pos_grip <= 45; pos_grip += 1) { //gripper close
-    // in steps of 1 degree
-    gripper.write(pos_grip);              // tell servo to go to position in variable 'pos_grip'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos_arm = 0; pos_arm <= 95; pos_arm += 1) { // arm goes from 0 degrees to 95 degrees
-    // in steps of 1 degree
-    gripper.write(pos_arm);              // tell servo to go to position in variable 'pos_grip'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos_grip = 45; pos_grip >= 0; pos_grip -= 1) { //gripper open
-    // in steps of 1 degree
-    gripper.write(pos_grip);              // tell servo to go to position in variable 'pos_grip'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  } 
-}#include<Servo.h>
-Servo leftServo;          // Define left servo
-Servo servoRight;         // Define right servo
-
-#include <QTRSensors.h>         //Pololu QTR Sensor Library
-
-
-/*------ Arduino Line Follower Code----- */
-/*-------definning Inputs------*/
-#define LS       // left sensor
-#define MS 9
-#define RS 8      // right sensor
-
-#define trigpin 12
-#define echo 13
-
-/*-------definning Outputs------*/
-#define LM 3       // left motor
-#define RM 2       // right motor
-
-//Create QTR Sensor Line Following Object
-//QTRSensorsRC qtrrc((unsigned char[]) {LS, MS, RS}, 3, 2500, QTR_NO_EMITTER_PIN);
-//unsigned int sensorValues[3];
-
-#define NUM_SENSORS             5  // number of sensors used
-#define NUM_SAMPLES_PER_SENSOR  4  // average 4 analog samples per sensor reading
-#define EMITTER_PIN             QTR_NO_EMITTER_PIN  // emitter is controlled by digital pin 2
-
-// sensors 0 through 5 are connected to analog inputs 0 through 5, respectively
-QTRSensorsAnalog qtra((unsigned char[]) {
-  0, 1, 2, 3, 4
-}, NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
-unsigned int sensorValues[NUM_SENSORS];
-
-void setup()
-{
-  leftServo.attach(LM);  // Set left servo to digital pin 4
-  servoRight.attach(RM);  // Set right servo to digital pin 3
-
-  Serial.begin(9600);    //Open serial port and set this baudrate
-
-  pinMode(trigpin, OUTPUT);
-  pinMode(echo, INPUT);
-
-  leftServo.write(90);
-  servoRight.write(90);
-
-  for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
-  {
-    qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
-  }
-
-  // print the calibration minimum values measured when emitters were on
-  Serial.begin(9600);
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
-    Serial.print(qtra.calibratedMinimumOn[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-
-  // print the calibration maximum values measured when emitters were on
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
-    Serial.print(qtra.calibratedMaximumOn[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
-  delay(1000);
-}
-
-int distance;
-void loop()
-{
-  unsigned int position = qtra.readLine(sensorValues);
-
-  if (position > 1500 && position < 2500) // position is around 1000
-  {
-    forward();
-    Serial.println ("Forward: ");
-    Serial.println(position);
-  }
-  else if (sensorValues[0] < 500 && sensorValues[2] < 500 && sensorValues[1] < 500) // stop if all are on white
-  {
-    //stopRobot ();
-    Serial.print("All white");
-  }
-  else if (sensorValues[0] > 500 && sensorValues[2] > 500 && sensorValues[1] > 500) // stop if all are on black
+  if (sensorValues[0] > 600 && sensorValues[1] > 600 && sensorValues[3] > 600 && sensorValues[4] > 600) // Identify Intersection
   {
     stopRobot ();
-    Serial.print("All black stop");
+    delay(2000);
+    Serial.print("Intersection");
+
+    turnLeft();
+    delay(2000);
+//    nextMove = decisions[decisionsCount];
+//    decisionsCount++;
+//    
+//    if (nextMove == 1) {
+//      forward();
+//    }
+//    if (nextMove == 2) {
+//      turnLeft();
+//      delay(150);
+//    }
+//    if (nextMove == 3) {
+//      turnRight();
+//      delay(150);
+//    }
   }
-  else if (position > 3500) // the line is on the left
+  if (position > 1800 && position < 2200 && sensorValues [0] < 500 && sensorValues [4]) // position is around 2000 go forward
+  {
+    forward();
+    Serial.println ("Forward: ");
+    Serial.println(position);
+  }
+  //  else if (sensorValues[0] > 500 && sensorValues[1] > 500 && sensorValues[2] < 500 && sensorValues[3] > 500 && sensorValues[4] > 500) // Identify T-Junction
+  //  {
+  //    stopRobot ();
+  //    Serial.print("T-Junction");
+  //
+  //    nextMove = decisions[decisionsCount];
+  //    decisionsCount ++;
+  //
+  //    if (nextMove == 2){
+  //      turnLeft();
+  //    }
+  //    if (nextMove == 3){
+  //      turnRight();
+  //    }
+  //  }
+  else if (position > 2200) // the line is on the left
   {
     turnRight();
     delay(150);
     Serial.println ("Left turn: " );
     Serial.println (position);
   }
-  else if (position < 2000)  // the line is on the right
+  else if (position < 1900)  // the line is on the right
   {
     turnLeft();
     delay(150);
     Serial.println ("Right turn: ");
     Serial.println(position);
-  }
+  }  
+  else if (sensorValues[0] < 500 && sensorValues[1] < 500 && sensorValues[2] < 500 && sensorValues[3] < 500 && sensorValues[4] < 500) //if all are on white
+  {
+//    //will either be a dead-end or the line is lost
+//    findDistance();
+//    if (distance <= 20)
+//    {
+//      stopRobot ();
+//      delay(150);
+//      deadEnd (); //reverse and turn 180 degrees because we are at a wall
+//    }
+//    else
+//    {
+//      forward();
+//      delay(3000);
+//      turnRight();
+//      delay(2000);
+//    }
+//    Serial.print("All white");
+   }
 }
 
 // Motion routines for forward, reverse, turns, and stop
@@ -345,14 +186,45 @@ void stopRobot() {
   servoRight.write(90);
 }
 
+//Distance to object using ultrasonic sensor
 void findDistance()
 {
-  int duration;
+  // Clears the trigpin
+  digitalWrite(trigpin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigpin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigpin, LOW);
-
+  // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echo, HIGH);
-  distance = (1 / 29.1) * (duration / 2);
-  delay(60);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+}
+
+//Pick up the cylinders
+void pickUp ()
+{
+
+}
+
+//At a dead end reverse and turn around 180 degrees
+void deadEnd ()
+{
+  stopRobot();
+  delay(500);
+  turnLeft();
+  delay(1000);
+  reverse();
+  delay (1000);
+  turnLeft ();
+  delay(2000);
+  reverse();
+  delay(800);
+  turnLeft();
+  delay(2000);
+  stopRobot();
 }
